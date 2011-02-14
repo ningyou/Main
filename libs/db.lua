4d7f9613dd63475c1d10e4b121394e0a127362c3
Stein-Ivar Berghei <stein-ivar@berghei.no>
2011-02-14 20:15:57 +0100

    Add a function to grab show titles based on _id.

--- libs/db.lua
+++ libs/db.lua
@@ -4,4 +4,14 @@ local _M = mongo.Connection.New()
 
 _M:connect"localhost"
 
+function _M:TitleFromID(id, type, lang or "main")
+	local q = self:query("ningyou.titles", { _id = id })
+
+	for r in q:results() do
+		return q.lang[lang]
+	end
+
+	return
+end
+
 return _M
