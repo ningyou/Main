646c914cd51beb13dab74ae6e524f1326693e776
Stein-Ivar Berghei <stein-ivar@berghei.no>
2011-02-16 00:04:08 +0100

    Move apikeys to mongodb.

--- libs/search.lua
+++ libs/search.lua
@@ -1,10 +1,17 @@
 require"json"
 require"lpeg"
 
+local db = require"db"
 local http = require"http"
 
--- Subject for change. Store these in mongo?
-local moviedb_apikey = io.open"moviedb_apikey":read"*all":gsub("\n$", "")
+local apikey = function(site)
+	local q = db:query("ningyou.apikey", { site = site })
+	for r in q:results() do
+		return r.site
+	end
+
+	return
+end
 
 local _M = {}
 
@@ -31,14 +38,14 @@ end
 function _M:moviedb(search)
 	-- IMDB id search.
 	if search:match("tt(%d+)") then
-		local movie, status = http:get("http://api.themoviedb.org/2.1/Movie.imdbLookup/en/json/" .. moviedb_apikey .. "/" .. search)
+		local movie, status = http:get("http://api.themoviedb.org/2.1/Movie.imdbLookup/en/json/" .. apikey"moviedb" .. "/" .. search)
 		if movie and status == 200 then
 			movie = json.decode(movie)
 			return movie
 		end
 	else
 		search = search:gsub("%s", "+")
-		local movie, status = http:get("http://api.themoviedb.org/2.1/Movie.search/en/json/" .. moviedb_apikey .. "/" .. search)
+		local movie, status = http:get("http://api.themoviedb.org/2.1/Movie.search/en/json/" .. apikey"moviedb" .. "/" .. search)
 		if movie and status == 200 then
 			movie = json.decode(movie)
 			return movie
