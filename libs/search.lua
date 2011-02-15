11912cff72b7aef3daaccbba9ce46c9658a8ecbd
Stein-Ivar Berghei <stein-ivar@berghei.no>
2011-02-15 23:45:40 +0100

    Add tvrage to the search lib

--- libs/search.lua
+++ libs/search.lua
@@ -1,4 +1,5 @@
 require"json"
+require"lpeg"
 
 local http = require"http"
 
@@ -6,6 +7,26 @@ local http = require"http"
 local moviedb_apikey = io.open"moviedb_apikey":read"*all":gsub("\n$", "")
 
 local _M = {}
+
+local split = function(s, sep)
+	sep = lpeg.P(sep)
+	local elem = lpeg.C((1 - sep)^0)
+	local p = lpeg.Ct(elem * (sep * elem)^0)
+
+	return lpeg.match(p, s)
+end
+
+local handleTVRage = function(str)
+	local data = {}
+
+	local tmp = split(str:gsub("\n", "@"), "@")
+	for i = 1, #tmp, 2 do
+		local k, v = tmp[i], tmp[i+1]
+		data[k] = v
+	end
+
+	return data
+end
 
 function _M:moviedb(search)
 	-- IMDB id search.
@@ -25,4 +46,18 @@ function _M:moviedb(search)
 	end
 end
 
+function _M:tvrage(search)
+	search = search:gsub("%s", "+")
+	local serie, status = http:get("http://services.tvrage.com/tools/quickinfo.php?show=" .. search)
+	if serie and status == 200 then
+		if serie:sub(1, 15) == "No Show Results" then
+			return
+		else
+			serie = serie:gsub("<pre>", "")
+			local data = handleTVRage(serie)
+
+			if data then return data else return end
+		end
+	end
+end
 return _M
