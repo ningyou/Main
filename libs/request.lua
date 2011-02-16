52017d799c6802e31703298fce4b4389de8ab90a
Trond A Ekseth <troeks@gmail.com>
2011-02-16 20:52:36 +0100

    A simple library for _GET/_POST which hopefully works.

---
+++ libs/request.lua
@@ -0,0 +1,43 @@
+local method = os.getenv'REQUEST_METHOD'
+local _GET ={}
+local _POST = {}
+
+local urlDecode
+do
+	local fmt = function(h) return string.char(tonumber(h,16)) end
+	function urlDecode(str)
+		if(str) then
+			str = str:gsub("+", " ")
+			str = str:gsub("%%(%x%x)", fmt)
+			str = str:gsub("\r\n", "\n")
+		end
+
+		return str
+	end
+end
+
+local parseRequest = function(str)
+	local request = {}
+	str:gsub('[^&]+', function(entry)
+		local key, value = entry:match'([^=]+)=(.*)$'
+		request[key] = urlDecode(value)
+		print(key, urlDecode(value))
+	end)
+
+	return request
+end
+
+local get_query = os.getenv'QUERY_STRING'
+if(#get_query > 0) then
+	_GET = parseRequest(get_query)
+end
+
+if(method == 'POST') then
+	_POST = parseRequest(io.read'*a')
+end
+
+return {
+	_POST = _POST,
+	_GET = _GET,
+	method = method,
+}
