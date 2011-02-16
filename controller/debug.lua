5ac5b39f52479afa556f3060ef4a7e6a95d8685b
Trond A Ekseth <troeks@gmail.com>
2011-02-16 20:55:54 +0100

    Add _POST and _GET output to debug.

--- controller/debug.lua
+++ controller/debug.lua
@@ -1,11 +1,25 @@
 local ob = require'ob'
 local cookies = require'cookies'
+local request = require'request'
 
 return {
 	index = function()
 		local h = assert(io.popen('env', 'r'))
 		local o = h:read'*all'
 
-		ob.Get'Content':write('<pre>', o, '</pre>')
+		ob.Get'Content':write'<pre>'
+		ob.Get'Content':write(o)
+
+		ob.Get'Content':write'_GET:'
+		for k,v in next, request._GET do
+			ob.Get'Content':write(k, ' = ', v)
+		end
+
+		ob.Get'Content':write'_POST:'
+		for k,v in next, request._POST do
+			ob.Get'Content':write(k, ' = ', v)
+		end
+
+		ob.Get'Conten':write'</pre>'
 	end,
 }
