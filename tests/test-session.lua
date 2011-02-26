7ca51178b1c75158f5467682c9a0f9d14ccfd03a
Stein-Ivar Berghei <stein-ivar@berghei.no>
2011-02-26 19:02:34 +0100

    Add a test for the session lib

---
+++ tests/test-session.lua
@@ -0,0 +1,13 @@
+local s = require'sessions'
+
+context('Library: Sessions', function()
+
+
+	test('can save and get session id\'s', function()
+		local id = "31337"
+		local oid = s:save(id)
+		assert_not_nil(oid)
+		assert_equal(s:get(oid), id)
+	end)
+
+end)
