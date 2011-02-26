972627e9c5549420b4441e9e0354048a99a4dc3b
Trond A Ekseth <troeks@gmail.com>
2011-02-26 15:21:08 +0100

    Update unit tests to reflect changes in the template system.

--- tests/test-template.lua
+++ tests/test-template.lua
@@ -5,16 +5,23 @@ context('Library: Template', function()
 	context('static template', function()
 		test('simple', function()
 			local input = "<p>Simple</p>"
-			local gen = template:Generate(input, _G)
+			local gen = template:Generate(input, true)
 
-			assert_equal(gen, "io.write[=[<p>Simple</p>]=]")
+			assert_equal(gen, "_O:write[=[<p>Simple</p>]=]")
 		end)
 
 		test('simple with newlines', function()
 			local input = "<p>Simple</p>\n<p>Simple</p>"
-			local gen = template:Generate(input, _G)
+			local gen = template:Generate(input, true)
 
-			assert_equal(gen, "io.write[=[<p>Simple</p>\n<p>Simple</p>]=]")
+			assert_equal(gen, "_O:write[=[<p>Simple</p>\n<p>Simple</p>]=]")
+		end)
+
+		test('simple non-minor template', function()
+			local input = "<p>Simple</p>"
+			local gen = template:Generate(input)
+
+			assert_equal(gen, "local ob = require'ob'\nlocal _O = ob.Get'Content'\n_O:write[=[<p>Simple</p>]=]")
 		end)
 	end)
 
@@ -22,62 +29,62 @@ context('Library: Template', function()
 		context('variable', function()
 			test('single', function()
 				local input = '{{ var }}'
-				local gen = template:Generate(input)
+				local gen = template:Generate(input, true)
 
-				assert_equal(gen, 'io.write(var)')
+				assert_equal(gen, '_O:write(var)')
 			end)
 
 			test('double without space', function()
 				local input = '{{ var }} {{ var }}'
-				local gen = template:Generate(input, setmetatable({var = 'variable'}, {__index = _G}))
+				local gen = template:Generate(input, true)
 
-				assert_equal(gen, 'io.write(var)\nio.write(var)')
+				assert_equal(gen, '_O:write(var)\n_O:write(var)')
 			end)
 
 			test('double with space', function()
 				local input = '{{ var }} {{ var }}'
-				local gen = template:Generate(input, setmetatable({var = 'variable'}, {__index = _G}))
+				local gen = template:Generate(input, true)
 
-				assert_equal(gen, 'io.write(var)\nio.write(var)')
+				assert_equal(gen, '_O:write(var)\n_O:write(var)')
 			end)
 
 			test('double with several spaces', function()
 				local input = '{{ var }}           {{ var }}'
-				local gen = template:Generate(input, setmetatable({var = 'variable'}, {__index = _G}))
+				local gen = template:Generate(input, true)
 
-				assert_equal(gen, 'io.write(var)\nio.write(var)')
+				assert_equal(gen, '_O:write(var)\n_O:write(var)')
 			end)
 		end)
 
 		context('snippet', function()
 			test('single line', function()
 				local input = '{% if(true) then end %}'
-				local gen = template:Generate(input, setmetatable({var = 'variable'}, {__index = _G}))
+				local gen = template:Generate(input, true)
 
 				assert_equal(gen, 'if(true) then end')
 			end)
 
 			test('multi line', function()
 				local input = '{% if(true) then\nend %}'
-				local gen = template:Generate(input, setmetatable({var = 'variable'}, {__index = _G}))
+				local gen = template:Generate(input, true)
 
 				assert_equal(gen, 'if(true) then\nend')
 			end)
 
 			test('multi line with html', function()
 				local input = '{% if(true) then\nlocal a = "test"\n %}\n<p>Clearly</p>\n{% print(a)\nend %}'
-				local gen = template:Generate(input, setmetatable({var = 'variable'}, {__index = _G}))
+				local gen = template:Generate(input, true)
 
-				assert_equal(gen, 'if(true) then\nlocal a = "test"\nio.write[=[<p>Clearly</p>]=]\nprint(a)\nend')
+				assert_equal(gen, 'if(true) then\nlocal a = "test"\n_O:write[=[<p>Clearly</p>]=]\nprint(a)\nend')
 			end)
 		end)
 
 		context('combined', function()
 			test('multi line with html and variable', function()
 				local input = '{% if(true) then\nlocal a = "test"\n %}\n{{ a }}\n{% print(a)\nend %}'
-				local gen = template:Generate(input, setmetatable({var = 'variable'}, {__index = _G}))
+				local gen = template:Generate(input, true)
 
-				assert_equal(gen, 'if(true) then\nlocal a = "test"\nio.write(a)\nprint(a)\nend')
+				assert_equal(gen, 'if(true) then\nlocal a = "test"\n_O:write(a)\nprint(a)\nend')
 			end)
 		end)
 	end)
