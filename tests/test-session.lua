dbf1a934f5b85592385ac27f47e82a6c25ad1b3b
Trond A Ekseth <troeks@gmail.com>
2011-02-28 15:57:16 +0100

    Empty lines be gone!

--- tests/test-session.lua
+++ tests/test-session.lua
@@ -1,13 +1,10 @@
 local s = require'sessions'
 
 context('Library: Sessions', function()
-
-
 	test('can save and get session id\'s', function()
 		local id = "31337"
 		local oid = s:save(id)
 		assert_not_nil(oid)
 		assert_equal(s:get(oid), id)
 	end)
-
 end)
