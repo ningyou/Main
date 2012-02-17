8333e279546cadb09e8f0b6d5712608c50af8400
Trond A Ekseth <troeks@gmail.com>
2012-02-18 00:30:20 +0100

    Redirect logged-in users to their dashboard.

--- controller/static.lua
+++ controller/static.lua
@@ -1,14 +1,17 @@
+local controller = require'controller'
 local template = require'template'
 local sessions = require'sessions'
 local user = require'user'
 
 return {
 	index = function()
-		local default_env = {
-			["user_id"] = sessions.user_id,
-			["user"] = user:Name(sessions.user_id),
-			["uri"] = "http://" .. getEnv()["Host"] .. (getEnv()['Path-Info'] or "/")
-		}
-		template:RenderView('default', nil, default_env)
+		if(sessions.user_id) then
+			controller:Call('user', nil, user:Name(sessions.user_id))
+		else
+			local default_env = {
+				["uri"] = "http://" .. getEnv()["Host"] .. (getEnv()['Path-Info'] or "/")
+			}
+			template:RenderView('default', nil, default_env)
+		end
 	end,
 }
