3b6dca5818661fda6d5daa41585eb4ec19a7d039
Stein-Ivar Berghei <stein-ivar@berghei.no>
2012-04-08 00:12:58 +0200

    Fix our anidbsearch libary somewhat

--- libs/anidbsearch.lua
+++ libs/anidbsearch.lua
@@ -5,7 +5,21 @@ local scores = {
 	official = 1,
 }
 
+local delims = " \t\r\n"
 local THRESHOLD = 100
+
+local function tokenize(text)
+	local tokens = { ["$and"] = {}}
+	local unique = {}
+	for token in text:gmatch("[^%s]+") do
+		if #token and not unique[token] and token:len() > 1 then
+			table.insert(tokens["$and"], { _keywords = token })
+			unique[token] = true
+		end
+	end
+	return tokens
+end
+
 local insert = function(tbl, aid, weight)
 	weight = math.floor(weight)
 	if(weight < THRESHOLD) then return end
@@ -28,23 +42,23 @@ local compare = function(tbl, aid, type, pattern, title)
 		return insert(tbl, aid, 1e3 * scores[type])
 	else
 		local x, y = title:find(pattern)
-		if(y) then
+		if y then
 			return insert(tbl, aid, 1e3 * (1 + y - x) / #title *  scores[type])
 		end
 	end
 end
 
 local doSearch = function(pattern)
+	local pattern = pattern:gsub("[^%w_0-9]+", " ")
 	local matches = {}
 	local search = pattern:lower():gsub('([-?]+)', '%%%1'):gsub("'", '`')
-	-- Search, lol!
-	for _, type in next, { 'main', 'syn', 'short', 'official' } do
-		local query = _DB:query("ningyou.anidbtitles", { type = type })
-		for r in query:results() do
-			compare(matches, r.anidb_id, r.type, search, r.title)
-		end
+	local tokens = tokenize(search)
+
+	local result = _DB:query("ningyou.anidbtitles", tokens)
+	for r in result:results() do
+		insert(matches, r.anidb_id, 1e3 * scores[r.type])
 	end
-	
+
 	local output = {}
 	for k,v in next, matches do
 		table.insert(output, {id = k, title = v[1], weight = v[2]})
