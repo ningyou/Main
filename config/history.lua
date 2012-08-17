6fc5ba3b154dbdc2917c0a0a992cdcb3188866e5
Stein-Ivar Berghei <stein-ivar@berghei.no>
2012-08-17 20:35:33 +0200

    Change episode to episodes for consistency

--- config/history.lua
+++ config/history.lua
@@ -3,7 +3,9 @@
 return {
 	update = {
 		episode = '[%s] Watched %s ep. %d',
+		episodes = '[%s] Watched %s ep. %d',
 		status = '[%s] Changed %s to %s',
+		rating = '[%s] Changed %s to %s',
 	},
 	add = {
 		show = '[%s] Added %s to %s',
